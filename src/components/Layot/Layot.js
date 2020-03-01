127f45c9981404c05b9bc77ba92083ebdf504470
Misha <mmoskalu@yukon.org.ua>
2020-03-01 21:57:53 +0200

    add sidedraw menu

--- src/components/Layot/Layot.js
+++ src/components/Layot/Layot.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layot.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layot = (props) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-)
+class Layot extends Component {
 
-export default layot;
+    state = {
+        showSideDrawer: true
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({ showSideDrawer: false });
+    }
+
+    drawerToggleClickedHandler = () => {
+        this.setState((prevState) => {
+            return { showSideDrawer: !prevState.showSideDrawer }
+        });
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar drawerToggleClicked={ this.drawerToggleClickedHandler }/>
+                <SideDrawer
+                    open={ this.state.showSideDrawer }
+                    closed={ this.sideDrawerClosedHandler }/>
+                <main className={ classes.Content }>
+                    { this.props.children }
+                </main>
+            </Aux>
+        )
+    }
+}
+
+export default Layot;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -5,11 +5,15 @@ import classes from './SideDrawer.css'
 import Backdrop from '../../UI/Backdrop/Backdrop';
 import Aux from '../../../hoc/Aux';
 
-const sideDrawer = () => {
+const sideDrawer = (props) => {
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
     return (
         <Aux>
-            <Backdrop />
-            <div className={classes.SideDrawer}>
+            <Backdrop show={props.open} clicked={props.closed} />
+            <div className={attachedClasses.join(' ')}>
                 <div className={classes.Logo}>
                     <Logo />
                 </div>
