eb8cef4674e4381c143061188c7582634b7860a3
misha <mmoskalu@yukon.org.ua>
2020-10-30 11:58:27 +0200

    refactor layout styles

--- src/hoc/Layot/Layot.js
+++ src/hoc/Layot/Layot.js
@@ -1,43 +1,43 @@
 import React, { Component } from 'react';
 import Aux from '../Aux/Aux';
-import classes from './Layot.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 import { withRouter } from 'react-router-dom';
 
 class Layot extends Component {
+  state = {
+    showSideDrawer: false,
+  };
 
-    state = {
-        showSideDrawer: false
-    }
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
 
-    sideDrawerClosedHandler = () => {
-        this.setState({ showSideDrawer: false });
-    }
+  drawerToggleClickedHandler = () => {
+    this.setState((prevState) => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+  };
 
-    drawerToggleClickedHandler = () => {
-        this.setState((prevState) => {
-            return { showSideDrawer: !prevState.showSideDrawer }
-        });
-    }
+  onLogoClickHandler = () => {
+    this.props.history.replace('/');
+  };
 
-    onLogoClickHandler = () => {
-        this.props.history.replace('/')
-    }
-
-    render() {
-        return (
-            <Aux>
-                <Toolbar clicked={this.onLogoClickHandler} drawerToggleClicked={ this.drawerToggleClickedHandler }/>
-                <SideDrawer
-                    open={ this.state.showSideDrawer }
-                    closed={ this.sideDrawerClosedHandler }/>
-                <main className={ classes.Content }>
-                    { this.props.children }
-                </main>
-            </Aux>
-        )
-    }
+  render() {
+    return (
+      <Aux>
+         <Toolbar
+            clicked={this.onLogoClickHandler}
+            drawerToggleClicked={this.drawerToggleClickedHandler}
+          />
+          <SideDrawer
+            open={this.state.showSideDrawer}
+            closed={this.sideDrawerClosedHandler}
+          />
+          <main>{this.props.children}</main>
+      </Aux>
+    );
+  }
 }
 
 export default withRouter(Layot);
